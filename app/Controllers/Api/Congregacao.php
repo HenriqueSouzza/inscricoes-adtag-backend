defbd32162a1bcc8773e68c245b75603694362f7
unknown <henriquehps1997@gmail.com>
2020-05-06 17:25:48 -0300

    criado a api de congregacao

--- app/Controllers/Api/Congregacao.php
+++ app/Controllers/Api/Congregacao.php
@@ -24,11 +24,91 @@ class Congregacao extends Controller
 
     public function index()
     {
+        $authorization = $this->request->getHeader('Authorization'); 
+
+        //Verifica se está passando algum token
+        if(!$authorization):
+
+            return $this->failNotFound('Error Not found');
+
+        endif;
+
+        //valida o token
+        $validateToken = $this->authorization->validateToken($authorization->getValue());
+
+        //caso o token não for válido, retorna uma erro para o usuário
+        if(!$validateToken):
+
+            return $this->failUnauthorized('Error Unauthorized');
+
+        endif;
+
+        $congregacao = $this->congregacao->paginate();
+        
+        return $this->respond($congregacao);
+
+        // header('content-type: text/html');
+        // $arquivo = file('C:\Users\Henrique\Downloads\congregacoes.txt');
+        // // echo '<pre>';
+        // // var_dump($arquivo);
+
+        // $array = [];
+
+        // foreach($arquivo as $key => $value):
+
+        //     $dado = explode(',', strval($value));
+
+        //     $array[] = [
+        //         'regiao' => (int) $dado[0],
+        //         'nome_congregacao' => $dado[1]
+        //     ];
+
+        // endforeach;
+
+        // foreach($array as $i => $val):
+        //     $erro = $this->congregacao->insert($val);
+        //     if(!$erro):
+        //         var_dump($this->congregacao->errors());die();
+        //         die();
+        //     endif;
+        // endforeach;
+        // echo '<pre>';
+        // var_dump($array);
+        // die();
 
     }
 
+    /**
+     * 
+     */
     public function show($id)
     {
+        $authorization = $this->request->getHeader('Authorization'); 
+
+        //Verifica se está passando algum token
+        if(!$authorization):
+
+            return $this->failNotFound('Error Not found');
+
+        endif;
+
+        //valida o token
+        $validateToken = $this->authorization->validateToken($authorization->getValue());
+
+        //caso o token não for válido, retorna uma erro para o usuário
+        if(!$validateToken):
+
+            return $this->failUnauthorized('Error Unauthorized');
+
+        endif;
+
+        $congregacao = $this->congregacao->find($id);
+
+        if(!$pessoa):
+            return $this->failNotFound('Error Not found');
+        endif;
+
+        return $this->respond($congregacao);
 
     }
 
--- app/Models/Congregacao.php
+++ app/Models/Congregacao.php
@@ -4,5 +4,29 @@ use CodeIgniter\Model;
 
 class Congregacao extends Model
 {
-    
+    protected $DBGroup = "default"; 
+
+    protected $table      = 'congregacao';
+    protected $primaryKey = 'congregacao';
+
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = [
+        'regiao', 
+        'nome_congregacao', 
+    ];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [
+        'regiao'            => 'required|is_natural', 
+        'nome_congregacao'  => 'required|min_length[5]', 
+    ];
+
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
 }
